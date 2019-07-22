b895846a5372412348cf790e77837c5932ca9044
Clement Polet <clpolet@microsoft.com>
2019-07-22 15:54:53 -0700

    Remove unused models

--- CaptainPlanet/Models/IConfidenceScore.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-namespace CaptainPlanet.Models
-{
-    public interface IConfidenceScore
-    {
-    }
-}
