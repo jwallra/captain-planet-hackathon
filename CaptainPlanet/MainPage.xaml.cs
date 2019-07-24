c48abaf72865b6bcad01cb2b11f6c18d6d9b3908
Clement Polet <clpolet@microsoft.com>
2019-07-23 18:31:55 -0700

    New UI from ObjectDetector

--- CaptainPlanet/MainPage.xaml.cs
+++ CaptainPlanet/MainPage.xaml.cs
@@ -8,104 +8,195 @@ using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
 
 namespace CaptainPlanet
 {
     public partial class MainPage : ContentPage
     {
-        // subscriptionKey.
-        private string subscriptionKey = AppSettingsManager.Settings["CognitiveServicesApiKey"];
-
-        private static readonly List<VisualFeatureTypes> features = new List<VisualFeatureTypes>()
-        {
-            VisualFeatureTypes.Categories,
-            VisualFeatureTypes.Objects,
-            VisualFeatureTypes.Tags
-        };
+        const float radius = 2.0f;
+        const float xDrop = 2.0f;
+        const float yDrop = 2.0f;
 
         public MainPage()
         {
             InitializeComponent();
-        }
 
-        private async void TakeAPicture(object sender, EventArgs e)
-        {
-            await CrossMedia.Current.Initialize();
-            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            var vm = (MainViewModel)BindingContext;
+            vm.PropertyChanged += (s, e) =>
             {
-                await DisplayAlert("No Camera", "No camera available.", "OK");
-                HidePicture();
-                return;
-            }
-            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-            {
-                Directory = "Sample",
-                Name = "xamarin.jpg",
-                MaxWidthHeight = 600,
-                PhotoSize = PhotoSize.MaxWidthHeight
-            });
-            await AnalyseFile(file);
-        }
-
-        private async void PickAPicture(object sender, EventArgs e)
-        {
-            await CrossMedia.Current.Initialize();
-
-            var file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
-            {
-                MaxWidthHeight = 600,
-                PhotoSize = PhotoSize.MaxWidthHeight
-            });
-            await AnalyseFile(file);
-        }
-
-        private async Task AnalyseFile(MediaFile file)
-        {
-            const string analysisFailedMessage = "Picture analysis failed.";
-            if (file == null) { 
-                HidePicture();
-                return;
-            }
-
-            try
-            {
-                ShowPicture();
-                chosenPicture.Source = ImageSource.FromStream(() =>
+                if (e.PropertyName == nameof(MainViewModel.Image) || e.PropertyName == nameof(vm.Predictions))
                 {
-                    var stream = file.GetStream();
-                    return stream;
-                });
-                var result = await GetImageDescription(file.GetStream());
-                file.Dispose();
-            }
-            catch (Exception ex)
+                    ImageCanvas.InvalidateSurface();
+                }
+            };
+        }
+
+        public void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            var vm = (MainViewModel)BindingContext;
+
+            var info = args.Info;
+            var canvas = args.Surface.Canvas;
+
+            ClearCanvas(info, canvas);
+
+            if (vm.Image != null)
             {
-                Debug.WriteLine($"{analysisFailedMessage}: {ex.Message}");
+                var scale = Math.Min((float)info.Width / (float)vm.Image.Width, (float)info.Height / (float)vm.Image.Height);
+
+                var scaleHeight = scale * vm.Image.Height;
+                var scaleWidth = scale * vm.Image.Width;
+
+                var top = (info.Height - scaleHeight) / 2;
+                var left = (info.Width - scaleWidth) / 2;
+
+                canvas.DrawBitmap(vm.Image, new SKRect(left, top, left + scaleWidth, top + scaleHeight));
+                DrawBorder(canvas, left, top, scaleWidth, scaleHeight);
+                DrawPredictions(vm, canvas, left, top, scaleWidth, scaleHeight);
             }
         }
 
-        private async Task<DetectResult> GetImageDescription(Stream imageStream)
+        static void DrawPredictions(MainViewModel vm, SKCanvas canvas, float left, float top, float scaleWidth, float scaleHeight)
         {
-            ComputerVisionClient computerVision = new ComputerVisionClient(
-                new ApiKeyServiceClientCredentials(subscriptionKey),
-                new System.Net.Http.DelegatingHandler[] { });
-            computerVision.Endpoint = AppSettingsManager.Settings["CognitiveServicesEndpoint"];
+            //if (vm.Predictions == null) return;
 
-            // Analyse.
-            return await computerVision.DetectObjectsInStreamAsync(imageStream);
+            //if (!vm.Predictions.Any())
+            //{
+            //    //LabelPrediction(canvas, "Nothing detected", new BoundingBox(0, 0, 1, 1), left, top, scaleWidth, scaleHeight, false);
+            //}
+            //else if (vm.Predictions.All(p => p.BoundingBox != null))
+            //{
+            //    foreach (var prediction in vm.Predictions)
+            //    {
+            //        //LabelPrediction(canvas, prediction.TagName, prediction.BoundingBox, left, top, scaleWidth, scaleHeight);
+            //    }
+            //}
+            //else
+            //{
+            //    var best = vm.Predictions.OrderByDescending(p => p.Probability).First();
+            //    //LabelPrediction(canvas, best.TagName, new BoundingBox(0, 0, 1, 1), left, top, scaleWidth, scaleHeight, false);
+            //}
         }
 
-        private void ShowPicture()
+        static void ClearCanvas(SKImageInfo info, SKCanvas canvas)
         {
-            noPictureText.IsVisible = false;
-            chosenPicture.IsVisible = true;
+            var paint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.White
+            };
+
+            canvas.DrawRect(info.Rect, paint);
         }
 
-        private void HidePicture()
+        //static void LabelPrediction(SKCanvas canvas, string tag, BoundingBox box, float left, float top, float width, float height, bool addBox = true)
+        //{
+        //    var scaledBoxLeft = left + (width * (float)box.Left);
+        //    var scaledBoxWidth = width * (float)box.Width;
+        //    var scaledBoxTop = top + (height * (float)box.Top);
+        //    var scaledBoxHeight = height * (float)box.Height;
+
+        //    if (addBox)
+        //        DrawBox(canvas, scaledBoxLeft, scaledBoxTop, scaledBoxWidth, scaledBoxHeight);
+
+        //    DrawText(canvas, tag, scaledBoxLeft, scaledBoxTop, scaledBoxWidth, scaledBoxHeight);
+        //}
+
+        static void DrawText(SKCanvas canvas, string tag, float startLeft, float startTop, float scaledBoxWidth, float scaledBoxHeight)
         {
-            chosenPicture.IsVisible = false;
-            noPictureText.IsVisible = true;
+            var textPaint = new SKPaint
+            {
+                IsAntialias = true,
+                Color = SKColors.White,
+                Style = SKPaintStyle.Fill,
+                Typeface = SKTypeface.FromFamilyName("Arial")
+            };
+
+            var text = tag;
+
+            var textWidth = textPaint.MeasureText(text);
+            textPaint.TextSize = 0.9f * scaledBoxWidth * textPaint.TextSize / textWidth;
+
+            var textBounds = new SKRect();
+            textPaint.MeasureText(text, ref textBounds);
+
+            var xText = (startLeft + (scaledBoxWidth / 2)) - textBounds.MidX;
+            var yText = (startTop + (scaledBoxHeight / 2)) + textBounds.MidY;
+
+            var paint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = new SKColor(0, 0, 0, 120)
+            };
+
+            var backgroundRect = textBounds;
+            backgroundRect.Offset(xText, yText);
+            backgroundRect.Inflate(10, 10);
+
+            canvas.DrawRoundRect(backgroundRect, 5, 5, paint);
+
+            canvas.DrawText(text,
+                            xText,
+                            yText,
+                            textPaint);
+        }
+
+        static void DrawBox(SKCanvas canvas, float startLeft, float startTop, float scaledBoxWidth, float scaledBoxHeight)
+        {
+            var strokePaint = new SKPaint
+            {
+                IsAntialias = true,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.White,
+                StrokeWidth = 5,
+                PathEffect = SKPathEffect.CreateDash(new[] { 20f, 20f }, 20f)
+            };
+            DrawBox(canvas, strokePaint, startLeft, startTop, scaledBoxWidth, scaledBoxHeight);
+
+            var blurStrokePaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = 5,
+                PathEffect = SKPathEffect.CreateDash(new[] { 20f, 20f }, 20f),
+                IsAntialias = true,
+                MaskFilter = SKMaskFilter.CreateBlur(SKBlurStyle.Normal, 0.57735f * radius + 0.5f)
+            };
+            DrawBox(canvas, blurStrokePaint, startLeft, startTop, scaledBoxWidth, scaledBoxHeight);
+        }
+
+        static void DrawBorder(SKCanvas canvas, float startLeft, float startTop, float scaledBoxWidth, float scaledBoxHeight)
+        {
+            var strokePaint = new SKPaint
+            {
+                IsAntialias = true,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.DarkSlateBlue,
+                StrokeWidth = 1
+            };
+
+            DrawBox(canvas, strokePaint, startLeft, startTop, scaledBoxWidth, scaledBoxHeight);
+        }
+
+        static void DrawBox(SKCanvas canvas, SKPaint paint, float startLeft, float startTop, float scaledBoxWidth, float scaledBoxHeight)
+        {
+            var path = CreateBoxPath(startLeft, startTop, scaledBoxWidth, scaledBoxHeight);
+            canvas.DrawPath(path, paint);
+        }
+
+        static SKPath CreateBoxPath(float startLeft, float startTop, float scaledBoxWidth, float scaledBoxHeight)
+        {
+            var path = new SKPath();
+            path.MoveTo(startLeft, startTop);
+
+            path.LineTo(startLeft + scaledBoxWidth, startTop);
+            path.LineTo(startLeft + scaledBoxWidth, startTop + scaledBoxHeight);
+            path.LineTo(startLeft, startTop + scaledBoxHeight);
+            path.LineTo(startLeft, startTop);
+
+            return path;
         }
     }
 }
---
+++ CaptainPlanet/MainViewModel.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using SkiaSharp;
+using Xamarin.Forms;
+
+namespace CaptainPlanet
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // subscriptionKey.
+        private string subscriptionKey = AppSettingsManager.Settings["CognitiveServicesApiKey"];
+
+        private static readonly List<VisualFeatureTypes> features = new List<VisualFeatureTypes>()
+        {
+            VisualFeatureTypes.Categories,
+            VisualFeatureTypes.Objects,
+            VisualFeatureTypes.Tags
+        };
+
+        public MainViewModel()
+        {
+#pragma warning disable RECS0165 // Asynchronous methods should return a Task instead of void
+            TakePhotoCommand = new Command(async () => await TakePhoto());
+            PickPhotoCommand = new Command(async () => await PickPhoto());
+#pragma warning restore RECS0165 // Asynchronous methods should return a Task instead of void
+        }
+
+        async Task PredictPhoto(MediaFile photo)
+        {
+            ComputerVisionClient computerVision = new ComputerVisionClient(
+                 new ApiKeyServiceClientCredentials(subscriptionKey),
+                 new System.Net.Http.DelegatingHandler[] { });
+            computerVision.Endpoint = AppSettingsManager.Settings["CognitiveServicesEndpoint"];
+
+            // Analyse.
+            var results = await computerVision.DetectObjectsInStreamAsync(photo.GetStream());
+            AllPredictions = results.Objects
+                                    .Where(p => p.Confidence > Probability)
+                                    .ToList();
+        }
+
+        SKBitmap image;
+        public SKBitmap Image
+        {
+            get => image;
+            set => Set(ref image, value);
+        }
+
+        double probability = .75;
+        public double Probability
+        {
+            get => probability;
+            set
+            {
+                if (Set(ref probability, value))
+                {
+                    OnPropertyChanged(nameof(ProbabilityText));
+                    OnPropertyChanged(nameof(Predictions));
+                }
+            }
+        }
+
+        public string ProbabilityText => $"{Probability:P0}";
+
+        List<DetectedObject> allPredictions = new List<DetectedObject>();
+        public List<DetectedObject> AllPredictions
+        {
+            get => allPredictions;
+            set
+            {
+                Set(ref allPredictions, value);
+                OnPropertyChanged(nameof(Predictions));
+            }
+        }
+
+        public List<DetectedObject> Predictions => AllPredictions.Where(p => p.Confidence > Probability).ToList();
+
+        public ICommand TakePhotoCommand { get; }
+        public ICommand PickPhotoCommand { get; }
+        public ICommand ShowSettingsCommand { get; }
+        public ICommand ShowHelpCommand { get;  }
+
+        Task TakePhoto()
+        {
+            return GetPhoto(() => CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions { PhotoSize = PhotoSize.Medium }));
+        }
+
+        Task PickPhoto()
+        {
+            return GetPhoto(() => CrossMedia.Current.PickPhotoAsync(new PickMediaOptions { PhotoSize = PhotoSize.Medium }));
+        }
+
+        async Task GetPhoto(Func<Task<MediaFile>> getPhotoFunc)
+        {
+            IsEnabled = false;
+
+            try
+            {
+                var photo = await getPhotoFunc();
+
+                Image = null;
+                AllPredictions = new List<DetectedObject>();
+
+                Image = SKBitmap.Decode(photo.GetStreamWithImageRotatedForExternalStorage());
+                await PredictPhoto(photo);
+
+                IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", $"An error occured: {ex.Message}", "OK");
+            }
+            finally
+            {
+                IsEnabled = true;
+            }
+        }
+    }
+}
